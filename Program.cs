17474acae0ae0e60c0b4ae6d1f88e4c2c1033b67
Игорь Коток <igormyhome@mail.ru>
2020-04-04 00:47:58 +0300

    Добавление алгоритма постороения массива граней суффиксов

--- Program.cs
+++ Program.cs
@@ -20,8 +20,10 @@ namespace BorderArray
                     " и построения массива граней *************\n");
                 // переменная в которую будут записываться данные из файла
                 String textInFile;
-                // объявление массива граней
+                // объявление массива граней префиксов
                 int[] masBP;
+                // объявление массива граней суффиксов
+                int[] masBS;
                 // считываем исходный текст из файла в переменную TextInFile
                 Console.WriteLine("Введите название файла в котором будет происходить поиск\n");
                
@@ -41,13 +43,21 @@ namespace BorderArray
 
                     }
                     masBP = new int[textInFile.Length];
-
-                    // демонстрация алгоритма по построению массива граней
+                    masBS = new int[textInFile.Length];
+                    // демонстрация алгоритма по построению массива граней префиксов
                     PrefixBorderArray(textInFile, masBP);
                     // вывод полученного массива граней
-                    Console.WriteLine("Массив граней: ");
+                    Console.WriteLine("Массив граней префиксов: ");
                     for (int i = 0; i < masBP.Length; i++)
                         Console.Write(masBP[i] + " ");
+                    Console.WriteLine();
+                    // демонстрация алгоритма по построению массива граней суффиксов
+                    SuffixBorderArray(textInFile, masBS);
+                    // вывод полученного массива граней
+                    Console.WriteLine("Массив граней суффиксов: ");
+                    for (int i = 0; i < masBS.Length; i++)
+                        Console.Write(masBS[i] + " ");
+
                     // демонстрация алгоритма поиска максимальной грани в строке
                     AlgorithmMaxBorder(textInFile);
                 }
@@ -68,8 +78,7 @@ namespace BorderArray
 
             }
 
-            //Алгоритм создания массива граней
-
+            // алгоритм создания массива граней префиксов
             static void PrefixBorderArray(string S, int[] bp)
             {
                 int n = S.Length;
@@ -88,8 +97,7 @@ namespace BorderArray
 
             }
 
-            // Алгоритм поиска максимальной грани
-
+            // алгоритм поиска максимальной грани
             static void AlgorithmMaxBorder(string textInFile)
             {
                 int n = textInFile.Length;
@@ -109,8 +117,21 @@ namespace BorderArray
                     Console.WriteLine("\nДлинна максимальной грани: " + br);
             }
 
+            // алгоритм построения массива суффиксов
+            static void SuffixBorderArray(string S, int[] bs)
+            {
+                int n = S.Length; ;
+                bs[n - 1] = 0;
+                for (int i = n - 2; i >= 0; --i)
+                {
+                    int bsLeft = bs[i + 1]; // Позиция с конца слева от предыдущей грани
+                    while (bsLeft != 0 && (S[i] != S[n - bsLeft - 1])) bsLeft = bs[n - bsLeft];
+                    // Длина на 1 больше, чем позиция
+                    if (S[i] == S[n - bsLeft - 1]) bs[i] = bsLeft + 1;
+                    else bs[i] = 0;
+                }
+            }
 
-           
 
 
 
